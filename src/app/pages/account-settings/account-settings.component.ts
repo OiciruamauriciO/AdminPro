858455e88c48b1b05edbf86eeeaabf1196c4dd02
OiciruamauriciO <mauroartistdeveloper@gmail.com>
2022-12-04 00:01:20 -0300

    Incorporación de lógica anterior a centralización de servicio de configuración setting

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from 'src/app/services/settings.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -11,35 +12,17 @@ export class AccountSettingsComponent implements OnInit {
   public linkTheme = document.querySelector('#theme');
   public links: NodeListOf<Element>;
 
-  constructor() {;
+  constructor(private settingService: SettingsService) {
     this.links = document.querySelectorAll('.selector');
   }
 
   ngOnInit(): void {
     this.links = document.querySelectorAll('.selector');
-    this.checkCurrentTheme();
   }
 
   changeTheme( theme: string){
-    const url = `./assets/css/colors/${ theme }.css`;
-    this.linkTheme?.setAttribute('href', url);
-    localStorage.setItem('theme', url);
-    this.checkCurrentTheme();
-  }
-
-  checkCurrentTheme(): void {
-
-    this.links.forEach( elem => {
-      elem.classList.remove('working');
-      const btnTheme = elem.getAttribute('data-theme');
-      const btnThemeUrl = `./assets/css/colors/${ btnTheme }.css`;
-      const currentTheme = this.linkTheme?.getAttribute('href');
-
-      if(btnThemeUrl === currentTheme){
-        elem.classList.add('working');
-      }
-
-    });
+    this.settingService.changeTheme(theme);
+    this.settingService.checkCurrentTheme(this.links);
   }
 
 }
