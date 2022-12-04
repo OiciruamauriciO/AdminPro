91a7dbcff88d57eee0497823a388bc9b7b06bbb1
OiciruamauriciO <mauroartistdeveloper@gmail.com>
2022-12-03 23:24:48 -0300

    Incorporación de lógica para identificar el theme seleccionado con clase working específicamente

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -9,18 +9,37 @@ import { Component, OnInit } from '@angular/core';
 export class AccountSettingsComponent implements OnInit {
 
   public linkTheme = document.querySelector('#theme');
+  public links: NodeListOf<Element>;
 
-  constructor() {
-
+  constructor() {;
+    this.links = document.querySelectorAll('.selector');
   }
 
   ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
   }
 
   changeTheme( theme: string){
     const url = `./assets/css/colors/${ theme }.css`;
     this.linkTheme?.setAttribute('href', url);
     localStorage.setItem('theme', url);
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme(): void {
+
+    this.links.forEach( elem => {
+      elem.classList.remove('working');
+      const btnTheme = elem.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${ btnTheme }.css`;
+      const currentTheme = this.linkTheme?.getAttribute('href');
+
+      if(btnThemeUrl === currentTheme){
+        elem.classList.add('working');
+      }
+
+    });
   }
 
 }
